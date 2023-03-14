deb26b4401b85dec908aa95daf59838661b18017
Olga Myakinina <olga.myakinina@icloud.com>
2023-03-14 23:25:47 +0300

    3 изменения
    1. Добавила блок-схему в README.md
    2. Удалила блок-схему в формате .png
    3. Добавила программу

---
+++ Zadacha/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+// Пример:
+// "hello", "2", ":-)"] -> ["2", ":-)"]
+
+string[] firstArray = new string[3] { "Hello", "2", "%-)" };
+string[] secondArray = new string[firstArray.Length];
+void NewArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] firstArray)
+{
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        Console.Write($"{firstArray[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(firstArray, secondArray);
+PrintArray(secondArray);
+
